d7e8458a64d929e3f8df63b7e9d18a03dc41d94b
Reisen_off <gleb.bstr@yandex.ru>
2024-05-31 10:55:01 +0300

    2 modification

--- client.py
+++ client.py
@@ -4,56 +4,80 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
-p = 23
-g = 5
-def generate_private_key():
-    return random.randint(1, p-1)
-def calculate_public_key(private_key):
-    return pow(g, private_key, p)
-def calculate_shared_secret(public_key, private_key):
-    return pow(public_key, private_key, p)
 
-def save_key_to_file(key, filename):
-    with open(filename, 'w') as file:
-        file.write(str(key))
+class DiffieHellman:
+    def __init__(self, p, g):
+        self.p = p
+        self.g = g
+        self.private_key = self.load_key_from_file('client_private_key.txt') or self.generate_private_key()
+        self.public_key = self.load_key_from_file('client_public_key.txt') or self.calculate_public_key()
+        self.save_key_to_file(self.private_key, 'client_private_key.txt')
+        self.save_key_to_file(self.public_key, 'client_public_key.txt')
 
-def load_key_from_file(filename):
-    if os.path.exists(filename):
-        with open(filename, 'r') as file:
-            return int(file.read())
-    return None
+    def generate_private_key(self):
+        return random.randint(1, self.p-1)
 
-def encrypt_message(message, key):
-    padder = padding.PKCS7(algorithms.AES.block_size).padder()
-    padded_data = padder.update(message.encode()) + padder.finalize()
-    iv = b'\x00' * 16  # Инициализационный вектор (может быть случайным)
-    iv = b'\x00' * 16
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    encryptor = cipher.encryptor()
-    return iv + encryptor.update(padded_data) + encryptor.finalize()
+    def calculate_public_key(self):
+        return pow(self.g, self.private_key, self.p)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 65432))
+    def calculate_shared_secret(self, public_key):
+        return pow(public_key, self.private_key, self.p)
 
-private_key = generate_private_key()
-public_key = calculate_public_key(private_key)
-private_key = load_key_from_file('client_private_key.txt')
-if private_key is None:
-    private_key = generate_private_key()
-    save_key_to_file(private_key, 'client_private_key.txt')
+    @staticmethod
+    def save_key_to_file(key, filename):
+        with open(filename, 'w') as file:
+            file.write(str(key))
 
-public_key = load_key_from_file('client_public_key.txt')
-if public_key is None:
-    public_key = calculate_public_key(private_key)
-    save_key_to_file(public_key, 'client_public_key.txt')
+    @staticmethod
+    def load_key_from_file(filename):
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                return int(file.read())
+        return None
 
-client_socket.sendall(str(public_key).encode())
+class AESCipher:
+    def __init__(self, key):
+        self.key = key
 
-server_public_key = int(client_socket.recv(1024).decode())
-print(f"Received server public key: {server_public_key}")
-shared_secret = calculate_shared_secret(server_public_key, private_key)
-print(f"Shared secret: {shared_secret}")
-message = "Hello, Secure World!"
-encrypted_message = encrypt_message(message, shared_secret.to_bytes(16, 'big'))
-client_socket.sendall(encrypted_message)
-client_socket.close()
+    def encrypt(self, message):
+        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+        padded_data = padder.update(message.encode()) + padder.finalize()
+        iv = b'\x00' * 16
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        return iv + encryptor.update(padded_data) + encryptor.finalize()
+
+    def decrypt(self, encrypted_message):
+        iv = encrypted_message[:16]
+        encrypted_message = encrypted_message[16:]
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        padded_data = decryptor.update(encrypted_message) + decryptor.finalize()
+        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+        return unpadder.update(padded_data) + unpadder.finalize()
+
+def main():
+    p = 23
+    g = 5
+    dh = DiffieHellman(p, g)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 65432))
+
+    client_socket.sendall(str(dh.public_key).encode())
+
+    server_public_key = int(client_socket.recv(1024).decode())
+    print(f"Received server public key: {server_public_key}")
+
+    shared_secret = dh.calculate_shared_secret(server_public_key)
+    print(f"Shared secret: {shared_secret}")
+
+    aes_cipher = AESCipher(shared_secret.to_bytes(16, 'big'))
+
+    message = "Hello, Secure World!"
+    encrypted_message = aes_cipher.encrypt(message)
+    client_socket.sendall(encrypted_message)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -4,71 +4,88 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
-p = 23
-g = 5
-def generate_private_key():
-    return random.randint(1, p-1)
-def calculate_public_key(private_key):
-    return pow(g, private_key, p)
-def calculate_shared_secret(public_key, private_key):
-    return pow(public_key, private_key, p)
 
-def save_key_to_file(key, filename):
-    with open(filename, 'w') as file:
-        file.write(str(key))
+class DiffieHellman:
+    def __init__(self, p, g):
+        self.p = p
+        self.g = g
+        self.private_key = self.load_key_from_file('server_private_key.txt') or self.generate_private_key()
+        self.public_key = self.load_key_from_file('server_public_key.txt') or self.calculate_public_key()
+        self.save_key_to_file(self.private_key, 'server_private_key.txt')
+        self.save_key_to_file(self.public_key, 'server_public_key.txt')
 
-def load_key_from_file(filename):
-    if os.path.exists(filename):
-        with open(filename, 'r') as file:
-            return int(file.read())
-    return None
+    def generate_private_key(self):
+        return random.randint(1, self.p-1)
 
-def encrypt_message(message, key):
-    padder = padding.PKCS7(algorithms.AES.block_size).padder()
-    padded_data = padder.update(message.encode()) + padder.finalize()
-    iv = b'\x00' * 16  # Инициализационный вектор (может быть случайным)
-    iv = b'\x00' * 16
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    encryptor = cipher.encryptor()
-    return iv + encryptor.update(padded_data) + encryptor.finalize()
-def decrypt_message(encrypted_message, key):
-    iv = encrypted_message[:16]
-    encrypted_message = encrypted_message[16:]
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    decryptor = cipher.decryptor()
-    padded_data = decryptor.update(encrypted_message) + decryptor.finalize()
-    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
-    return unpadder.update(padded_data) + unpadder.finalize()
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 65432))
-server_socket.listen(1)
+    def calculate_public_key(self):
+        return pow(self.g, self.private_key, self.p)
 
-print("Server is listening on port 65432")
+    def calculate_shared_secret(self, public_key):
+        return pow(public_key, self.private_key, self.p)
 
-private_key = load_key_from_file('server_private_key.txt')
-if private_key is None:
-    private_key = generate_private_key()
-    save_key_to_file(private_key, 'server_private_key.txt')
+    @staticmethod
+    def save_key_to_file(key, filename):
+        with open(filename, 'w') as file:
+            file.write(str(key))
 
-public_key = load_key_from_file('server_public_key.txt')
-if public_key is None:
-    public_key = calculate_public_key(private_key)
-    save_key_to_file(public_key, 'server_public_key.txt')
+    @staticmethod
+    def load_key_from_file(filename):
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                return int(file.read())
+        return None
 
-conn, addr = server_socket.accept()
-print(f"Connected by {addr}")
+class AESCipher:
+    def __init__(self, key):
+        self.key = key
 
-private_key = generate_private_key()
-public_key = calculate_public_key(private_key)
+    def encrypt(self, message):
+        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+        padded_data = padder.update(message.encode()) + padder.finalize()
+        iv = b'\x00' * 16
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        return iv + encryptor.update(padded_data) + encryptor.finalize()
 
-client_public_key = int(conn.recv(1024).decode())
-print(f"Received client public key: {client_public_key}")
+    def decrypt(self, encrypted_message):
+        iv = encrypted_message[:16]
+        encrypted_message = encrypted_message[16:]
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        padded_data = decryptor.update(encrypted_message) + decryptor.finalize()
+        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+        return unpadder.update(padded_data) + unpadder.finalize()
 
-conn.sendall(str(public_key).encode())
-shared_secret = calculate_shared_secret(client_public_key, private_key)
-print(f"Shared secret: {shared_secret}")
-encrypted_message = conn.recv(1024)
-print(f"Encrypted message received: {encrypted_message}")
-decrypted_message = decrypt_message(encrypted_message, shared_secret.to_bytes(16, 'big'))
-print(f"Decrypted message: {decrypted_message.decode()}")
-conn.close()
+def main():
+    p = 23
+    g = 5
+    dh = DiffieHellman(p, g)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 65432))
+    server_socket.listen(1)
+
+    print("Server is listening on port 65432")
+
+    conn, addr = server_socket.accept()
+    print(f"Connected by {addr}")
+
+    client_public_key = int(conn.recv(1024).decode())
+    print(f"Received client public key: {client_public_key}")
+
+    conn.sendall(str(dh.public_key).encode())
+
+    shared_secret = dh.calculate_shared_secret(client_public_key)
+    print(f"Shared secret: {shared_secret}")
+
+    aes_cipher = AESCipher(shared_secret.to_bytes(16, 'big'))
+
+    encrypted_message = conn.recv(1024)
+    print(f"Encrypted message received: {encrypted_message}")
+
+    decrypted_message = aes_cipher.decrypt(encrypted_message)
+    print(f"Decrypted message: {decrypted_message.decode()}")
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
